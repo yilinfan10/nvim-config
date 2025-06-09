6b49998e52fb4b7a0507d3581a380bf830e5dfcb
Yilin Fan <yilinf@nvidia.com>
2025-06-08 20:56:24 -0700

    Enable lspsage

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -82,4 +82,14 @@ return {
   {
     "airblade/vim-gitgutter",
   },
+  {
+    'nvimdev/lspsaga.nvim',
+    config = function()
+        require('lspsaga').setup({})
+    end,
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter', -- optional
+        'nvim-tree/nvim-web-devicons',     -- optional
+    }
+  }
 }
